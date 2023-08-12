b19047217f86f2247bcce453f65d8c5dbf57c132
scorpio <muhammedlameesmv@gmail.com>
2023-08-12 12:37:13 +0530

    first commit

---
+++ src/components/Auth.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { GoogleProvider, auth } from '../config/firebase'
+import { createUserWithEmailAndPassword,signInWithPopup, signOut } from 'firebase/auth'
+function Auth() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  console.log(auth?.currentUser?.photoURL);
+  console.log(auth?.currentUser?.email);
+  
+  const handleSignIn = async () => {
+    try{
+      await createUserWithEmailAndPassword(auth, email, password)
+    }catch (err) {
+      console.error(err);
+    }
+  };
+
+  const SingInWithGoogle = async () => {
+    try{
+      await signInWithPopup(auth, GoogleProvider)
+    }catch (err) {
+      console.error(err);
+    }
+  };
+
+  const SignOut = async () => {
+    try{
+      await signOut(auth)
+    }catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  return (
+    <div>
+      <input onChange={(e) => setEmail(e.target.value)} placeholder="email..." type="text" />
+      <input onChange={(e) => setPassword(e.target.value)} placeholder="passwod..." type="password" />
+      <button onClick={handleSignIn}>Sing in </button>
+      <button onClick={SingInWithGoogle}>Sing in With Google </button>
+      <button onClick={SignOut}>Log Out </button>
+    </div>
+  )
+}
+
+export default Auth
---
+++ src/config/firebase.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCI0fLKDXuK0T8wOh2Odj6U0HNzgGN2QUk",
+  authDomain: "pedrotech-bc305.firebaseapp.com",
+  projectId: "pedrotech-bc305",
+  storageBucket: "pedrotech-bc305.appspot.com",
+  messagingSenderId: "447516910070",
+  appId: "1:447516910070:web:85252f176a66a6d98867d7",
+  measurementId: "G-1TFVQCGP6F"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const GoogleProvider = new GoogleAuthProvider(app)
+
+export const db = getFirestore(app) 
